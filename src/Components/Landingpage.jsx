6a54b24e12cdc9ca8b4452cc8419b4899f083e7a
OthiRaja <othiraja5677@gmail.com>
2024-07-09 18:11:37 +0530

    admin

--- src/Components/Landingpage.jsx
+++ src/Components/Landingpage.jsx
@@ -1,30 +1,37 @@
-import {React,useEffect }from 'react';
-import { Container } from 'react-bootstrap'; 
+import { React, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 import '../Components/Landingpg.css'
 import Navbar from './Navbar';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
+import Admin from './admin/Admin'
+import { Routes, Route, useLocation } from 'react-router-dom';
 function LandingPG() {
-   // aos animation 
-   useEffect(() => {
+  // aos animation 
+  useEffect(() => {
     AOS.init({
       duration: 1000, // Animation duration
     });
   }, []);
-
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === '/admin') {
+      window.open('/Admin', '_blank');
+    }
+  }, [location]);
   return (
-  <div className='landing-pg bg-black'>
-
+    <div className='landing-pg bg-black'>
       <div className="background-image ">
-      <Navbar/>
-      <Container className="text-center text-overlay ">
-        <p className='LP-text1' data-aos='fade-in'  data-aos-delay='200'>Free Fuel Giveaway Everyday.</p>
-        <p className='LP-text2' data-aos='fade-in' data-aos-delay='300'>UrbanRider x Go Extra Mile</p>
-      </Container>
+        <Navbar />
+        <Routes>
+          <Route path="/Admin" element={<Admin />} />
+        </Routes>
+        <Container className="text-center text-overlay ">
+          <p className='LP-text1' data-aos='fade-in' data-aos-delay='200'>Free Fuel Giveaway Everyday.</p>
+          <p className='LP-text2' data-aos='fade-in' data-aos-delay='300'>UrbanRider x Go Extra Mile</p>
+        </Container>
+      </div>
     </div>
-  </div>
   );
 }
-
 export default LandingPG;
